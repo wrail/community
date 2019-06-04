cf3b9d521158b8554454065562d16d70d06aa83d
Wrial <2806935450@qq.com>
2019-06-04 13:30:28 +0800

    增加了gitHub获取User,并抽取出来

--- src/main/java/com/wrial/community/community/controller/AuthorizeController.java
+++ src/main/java/com/wrial/community/community/controller/AuthorizeController.java
@@ -4,18 +4,27 @@ package com.wrial.community.community.controller;
 import com.wrial.community.community.dto.AccessTokenDTO;
 import com.wrial.community.community.dto.GitHubUser;
 import com.wrial.community.community.provider.GitHubProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
 
 
     @Autowired
     GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String client_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
 
     //没返回说明CallBack失败???,原来是大小写问题
     @GetMapping("/callback")
@@ -23,15 +32,15 @@ public class AuthorizeController {
                            @RequestParam(name = "state") String state) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("70e24ea8682a92b4239f");
-        accessTokenDTO.setClient_secret("3c03dc4d0c5b9c6e04f1f5b2546121a27813add2");
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
 
+        log.info("打印出从github拿的user信息{}",user);
         return "index";
     }
 
--- src/main/java/com/wrial/community/community/provider/GitHubProvider.java
+++ src/main/java/com/wrial/community/community/provider/GitHubProvider.java
@@ -4,6 +4,7 @@ package com.wrial.community.community.provider;
 import com.alibaba.fastjson.JSON;
 import com.wrial.community.community.dto.AccessTokenDTO;
 import com.wrial.community.community.dto.GitHubUser;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import java.io.IOException;
 
 //Provider:提供一些第三方支持
 @Component
+@Slf4j
 public class GitHubProvider {
 
 
@@ -34,7 +36,8 @@ public class GitHubProvider {
             //  System.out.println("Response:" + s);
             //可以从输出看到返回的信息access_token=857635261a8ca5bad6bec2366c3a26510bcc6200&scope=user&token_type=bearer
             //只需要token的值,并返回
-            String token =  s.split("&")[0].split("=")[1];
+            String token = s.split("&")[0].split("=")[1];
+           log.info("打印出token"+token);
             return token;
 
         } catch (Exception e) {
@@ -48,13 +51,13 @@ public class GitHubProvider {
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://github.com/login/oauth/access_token" + accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
             String s = response.body().string();
             //会自动将Json转换为类对象
-
+            log.info("打印出响应的user信息{}",s);
 
             //此处有问题
             GitHubUser gitHubUser = JSON.parseObject(s, GitHubUser.class);
