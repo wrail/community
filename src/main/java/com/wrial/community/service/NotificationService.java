1fa304061af668a4dac494f6c217b70727bf3bb3
wrail <2806935450@qq.com>
2019-08-03 10:56:49 +0800

    修改遗留问题：重写sql并让未读消息desc

--- src/main/java/com/wrial/community/service/NotificationService.java
+++ src/main/java/com/wrial/community/service/NotificationService.java
@@ -15,6 +15,7 @@ import com.wrial.community.mapper.NotificationMapper;
 import com.wrial.community.mapper.UserMapper;
 import com.wrial.community.model.Notification;
 import com.wrial.community.model.User;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,8 +43,15 @@ public class NotificationService {
         if (offset < 0) {
             offset = 0;
         }
+
+        Example example = new Example(Notification.class);
+        example.createCriteria().andEqualTo("receiver", userId);
+        example.setOrderByClause("gmt_create desc");
+
+        List<Notification> notifications = notificationMapper.selectByExampleAndRowBounds(example, new RowBounds(offset, size));
+
         //limit分页
-        List<Notification> notifications = notificationMapper.selectPage(offset, size);
+//        List<Notification> notifications = notificationMapper.selectPage(offset, size);
 
         List<NotificationDTO> notificationDTOS = new ArrayList<>();
         //将分页查询出的数据进一步封装到dto里
@@ -55,15 +63,15 @@ public class NotificationService {
             notificationDTOS.add(notificationDTO);
         }
         paginationDTO.setData(notificationDTOS);
-        Integer totalCount =notificationMapper.count(userId);
+        Integer totalCount = notificationMapper.count(userId);
         paginationDTO.setPagination(totalCount, page, size);
         return paginationDTO;
     }
 
-    public int unRead(Long receiverId){
+    public int unRead(Long receiverId) {
         Example notificationExample = new Example(Notification.class);
         notificationExample.createCriteria()
-                .andEqualTo("receiver",receiverId)
+                .andEqualTo("receiver", receiverId)
                 .andEqualTo("status", NotificationStatusEnum.UNREAD.getStatus());
         return notificationMapper.selectCountByExample(notificationExample);
 
@@ -76,16 +84,16 @@ public class NotificationService {
         Example example = new Example(Notification.class);
         example.createCriteria().andEqualTo("id", id);
         Notification notification = notificationMapper.selectOneByExample(example);
-        if (notification==null){
+        if (notification == null) {
             throw new CustomizeException(CustomizeErrorCode.NOTIFICATION_NOT_FOUND);
         }
-        if (notification.getReceiver()!=user.getId()){
+        if (notification.getReceiver() != user.getId()) {
             throw new CustomizeException(CustomizeErrorCode.READ_NOTIFICATION_FIAL);
         }
 
         notification.setStatus(NotificationStatusEnum.READ.getStatus());
         //更新状态
-        notificationMapper.updateStatus(notification.getId(),notification.getStatus());
+        notificationMapper.updateStatus(notification.getId(), notification.getStatus());
 
         NotificationDTO notificationDTO = new NotificationDTO();
         BeanUtils.copyProperties(notification, notificationDTO);
